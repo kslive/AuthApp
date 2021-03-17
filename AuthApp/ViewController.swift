71723b21fc241d01c48ae85f5265a23690bd7075
Eugene Kiselev <zhenya-kiselev-97@mail.ru>
2021-03-17 09:45:56 +0300

    Initial Commit

---
+++ AuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthApp
+//
+//  Created by Eugene Kiselev on 17.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
